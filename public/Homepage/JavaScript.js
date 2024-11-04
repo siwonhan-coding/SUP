458bd97ac8237821ebc705cfa77f96465c828551
siwonhan-coding <mercoding@gmail.com>
2024-11-04 13:55:16 -0500

    added padding and clean

--- public/Homepage/JavaScript.js
+++ public/Homepage/JavaScript.js
@@ -54,40 +54,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-// document.addEventListener("DOMContentLoaded", function () {
-//   const open = document.querySelector(".container");
-//   const close = document.querySelector(".close");
-
-// GSAP Timeline
-//   var tl = gsap.timeline({
-//     paused: true,
-//     reversed: true,
-//     defaults: { duration: 0.5, ease: "expo.inOut" },
-//   });
-
-//   tl.to("nav", { right: 0 }) // Move nav into view
-//     .to("nav", { height: "100vh" }, "-=0.1") // Expand to full height
-//     .to(
-//       "nav ul li a",
-//       { opacity: 1, pointerEvents: "all", stagger: 0.5 }, // Show menu items
-//       "-=0.8"
-//     )
-//     .to(".close", { opacity: 1, pointerEvents: "all" }, "-=0.2") // Show close button
-//     .to("nav h2", { opacity: 1 }, "-=1"); // Show heading
-
-//   // Open button click event
-//   open.addEventListener("click", () => {
-//     if (tl.reversed()) {
-//       tl.play(); // Play animation
-//     }
-//   });
-
-//   // Close button click event
-//   close.addEventListener("click", () => {
-//     tl.reverse(); // Reverse animation
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   const hiddenElements = document.querySelectorAll(".hidden"); // Ensure 'hidden' elements are selected properly
 
@@ -147,21 +113,3 @@ const getPos = function (current, active) {
   return diff;
 };
 
-// Function to create bubbles dynamically
-function createBubbles() {
-  const bubbleContainer = document.querySelector(".bubbles");
-  for (let i = 0; i < 128; i++) {
-    // Create 128 bubbles
-    const bubble = document.createElement("div");
-    bubble.classList.add("bubble");
-    bubble.style.setProperty("--size", `${2 + Math.random() * 4}rem`);
-    bubble.style.setProperty("--distance", `${6 + Math.random() * 4}rem`);
-    bubble.style.setProperty("--position", `${-5 + Math.random() * 110}%`);
-    bubble.style.setProperty("--time", `${2 + Math.random() * 2}s`);
-    bubble.style.setProperty("--delay", `${-1 * (2 + Math.random() * 2)}s`);
-    bubbleContainer.appendChild(bubble);
-  }
-}
-
-// Call the function to create bubbles
-createBubbles();
